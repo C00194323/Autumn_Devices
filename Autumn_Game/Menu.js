d3a29c86a6aac4bf5854bf47e0159b9d1de71e3f
Sean <kearnsy8@gmail.com>
2017-08-21 18:45:15 +0100

    Background changed, Settings Changed

--- Autumn_Game/Menu.js
+++ Autumn_Game/Menu.js
@@ -13,7 +13,7 @@ Menu.prototype.init= function() {
   this.winWidth =(window.innerWidth)/2;
 
 	this.Background = new Image();
-	this.Background.src = 'assets/Menu_Assets/Menu_Background.jpg';
+	this.Background.src = 'assets/Menu_Assets/Background.jpg';
 
 	this.PlayButton = new Image();
 	this.PlayButton.src = 'assets/Menu_Assets/play_button.png';
@@ -24,6 +24,8 @@ Menu.prototype.init= function() {
 	this.exitButton = new Image();
 	this.exitButton.src = 'assets/Menu_Assets/exit_button.png';
 
+
+
 };
 
 Menu.prototype.draw= function(ctx) {
@@ -33,4 +35,9 @@ Menu.prototype.draw= function(ctx) {
 	app.ctx.drawImage(this.PlayButton,this.winWidth-65,this.winHeight,130,130);
 	app.ctx.drawImage(this.OptionsButton,this.winWidth-65,this.winHeight+200,130,130);
 	app.ctx.drawImage(this.exitButton,this.winWidth-65,this.winHeight+400,130,130);
+
+	app.ctx.fillStyle = rgb(0,0,0);
+	app.ctx.font = 'italic 52pt Calibri';
+	app.ctx.TextBaseline = "top";
+	app.ctx.fillText("Princess Capture" , app.canvas.width/4,app.canvas.height/5);
 };
--- Autumn_Game/SettingMenu.js
+++ Autumn_Game/SettingMenu.js
@@ -9,11 +9,12 @@ function SettingMenu()
   var soundDownHeight;
   var textheight;
   var textWidth;
+  var exitButton;
 }
 
 SettingMenu.prototype.init= function(){
   this.settingBackground = new Image();
-  this.settingBackground.src = 'assets/SettingMenu_Assets/SettingMenuBackground.png';
+  this.settingBackground.src = 'assets/SettingMenu_Assets/SettingBackground.jpg';
 
   this.soundUp=new Image();
   this.soundUp.src='assets/SettingMenu_Assets/SoundUp.png';
@@ -30,17 +31,27 @@ SettingMenu.prototype.init= function(){
   this.textheight= window.innerHeight/4;
   this.textWidth= window.innerWidth/6;
 
+  this.exitButton = new Image();
+  this.exitButton.src='assets/SettingMenu_Assets/Exit.png';
+
 }
 
 SettingMenu.prototype.draw= function(ctx){
 
   app.ctx.drawImage(this.settingBackground,0,0,app.canvas.width,app.canvas.height);
 
-  app.ctx.fillStyle = rgb(256,256,256);
+  app.ctx.fillStyle = rgb(0,0,0);
 	app.ctx.font = 'italic 40pt Calibri';
 	app.ctx.TextBaseline = "top";
-	app.ctx.fillText("Sound:" , this.textWidth,this.textheight);
+	app.ctx.fillText("Sound :", this.textWidth,this.textheight);
+  app.ctx.fillText(" Exit :", app.canvas.width/3.6,((app.canvas.height/2)*1.6))
+
+  app.ctx.font = 'italic 52pt Calibri';
+	app.ctx.TextBaseline = "top";
+	app.ctx.fillText("Settings" , app.canvas.width/3,app.canvas.height/6);
 
   app.ctx.drawImage(this.soundUp,this.soundUpWidth,this.soundUpHeight);
-    app.ctx.drawImage(this.soundDown,this.soundDownWidth,this.soundDownHeight);
+  app.ctx.drawImage(this.soundDown,this.soundDownWidth,this.soundDownHeight);
+
+  app.ctx.drawImage(this.exitButton,app.canvas.width/2.4,((app.canvas.height/2)*1.5))
 }
